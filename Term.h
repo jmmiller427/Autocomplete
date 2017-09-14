b0160c713498c9da90e760363644d5e358029d08
John Miller <jmmi234@g.uky.edu>
2017-09-13 20:15:22 -0400

    Update Term.h

--- Term.h
+++ Term.h
@@ -10,25 +10,24 @@ using namespace std;
 class Term
 {
 	public:
-// term constructors
 		Term();
 		Term(string query, long weight);
-// reverse weight order
+
 		int byReverseWeightOrder(Term term1, Term term2);
-// compare function
+
 		int compareTo(Term term1, Term term2);
-// prefix order function
+
 		int byPrefixOrder(Term term1, Term term2, int r);
-// print function
+
 		void print();
-// get weight function
+
 		int getWeight();
-// get query function
+
 		string getQuery();
-// make autocomplete a friend class
+
 		friend class Autocomplete;
 		friend ostream &operator<<(ostream &out, Term &T);
-// make private variables
+
 	private:
 		string query;
 		long weight;
