c30f15af4b6aa03c7be3227149afa8bef407608b
John Miller <jmmi234@g.uky.edu>
2017-09-13 20:18:39 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,44 +12,33 @@ using namespace std;
 // main file with command line arguments
 int main(int argc, char** argv)
 {
-// make a bool if the key was found in the file
 	bool found_in_file = true;
-// make a string called value
 	string value;
-// ifstream
+	string line;
 	ifstream in_file;
-// make an autocomplete object
+
 	Autocomplete autocomplete;
 
 // if argument number in command line != 3
 	if (argc != 3)
 	{
-// output there needs to be three arguments
 		cout << "Please enter 3 command line arguments" << endl;
 		return 1;
 	}
 
-// make a result argument to integer as argument 2 in command line
 	int result = atoi(argv[2]);
 
-// open the file as argument 1 in command line
 	in_file.open(argv[1]);
 
 // if the file fails to open
 	if (in_file.fail())
 	{
-// output the file was not found
 		cout << "File not found" << endl;
 		return 2;
 	}
-
-// make a string called line
-	string line;
-
-// do while loop 
+	
 	do 
 	{
-// while loop and getline
 		while (getline(in_file, line))
 		{
 // long value for weight that finds the tab 
@@ -59,20 +48,15 @@ int main(int argc, char** argv)
 // erase the query
 			query = query.erase(query.find('\t'), query.find_first_not_of(" \t\r\n"));
 
-// make a term object with query and weight
 			Term Term(query, weight);
 // call insert function with term argument
 			autocomplete.insert(Term);
 		}
-// output to enter a value
 		cout << "Please enter a search value (exit to quit): " << endl;
-// get the input value as the key
 		cin >> value;
 
-// if the value is exit
 		if (value == "exit")
 		{
-// exit the loop
 			found_in_file = false;
 			continue;
 		}
