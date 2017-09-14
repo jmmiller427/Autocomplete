b9e548fe6a87a836a814dbf7f35e6fc2f441cddf
John Miller <jmmi234@g.uky.edu>
2017-09-13 20:08:19 -0400

    Update Autocomplete.h

--- Autocomplete.h
+++ Autocomplete.h
@@ -12,21 +12,18 @@ using namespace std;
 class Autocomplete
 {
 	public:
-// autocomplete constructors
 		Autocomplete();
-// insert finction
+
 		void insert(Term newterm);
-// search function
+
 		void Search(string key, int num);
-// BS helper
+
 		int BS_helper(string key, int left, int right);
-// binary search function
-		/*void print*/
-// lexicographical order function
+
 		void Lexicographical(long left, long right);
-// sort weight function
+
 		void weightSort();
-// get the size of the vector
+
 		int getVectorSize();
 
 	private:
